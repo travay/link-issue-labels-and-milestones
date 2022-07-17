a98eb44f00eb7ba896d0da26bacb97d20993af73
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-07-17 14:49:47 +0200

    updated file to test the result of the new formed GQL query

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ const main = async () => {
     // Get labels and milestones of issues linked to the PR
     const { queryString, queryUrl } = linkedLabelsAndMilestones(pr_number);
 
-    const { resource }: LinkedLabelsAndMilestonesData = await graphql({
+    const  resource : LinkedLabelsAndMilestonesData = await graphql({
       query: queryString,
       queryUrl,
       headers: {
@@ -30,38 +30,40 @@ const main = async () => {
       throw Error("Could not find linked issues");
     }
 
+    console.log(resource)
+
     // Find all labels
-    resource.closingIssuesReferences.nodes.forEach((issue) => {
-      issue.labels.edges.forEach((issueLabels) =>
-        labels.push(issueLabels.node.name)
-      );
-    });
+    // resource.closingIssuesReferences.nodes.forEach((issue) => {
+    //   issue.labels.edges.forEach((issueLabels) =>
+    //     labels.push(issueLabels.node.name)
+    //   );
+    // });
 
-    // Find all milestones
-    resource.closingIssuesReferences.nodes.forEach((issue) => {
-      milestones.push(issue.milestone.number);
-    });
+    // // Find all milestones
+    // resource.closingIssuesReferences.nodes.forEach((issue) => {
+    //   milestones.push(issue.milestone.number);
+    // });
 
-    if (labels.length === 0) {
-      throw Error(
-        "Linked issue has no labels, please make sure to appropriately label the issue linked to this PR."
-      );
-    }
+    // if (labels.length === 0) {
+    //   throw Error(
+    //     "Linked issue has no labels, please make sure to appropriately label the issue linked to this PR."
+    //   );
+    // }
 
-    if (milestones.length === 0) {
-      throw Error(
-        "Linked issue has no milestone, please make sure to add a milestone to the issue linked to this PR."
-      );
-    }
+    // if (milestones.length === 0) {
+    //   throw Error(
+    //     "Linked issue has no milestone, please make sure to add a milestone to the issue linked to this PR."
+    //   );
+    // }
 
-    // Add milestone to PR
-    await octokit.rest.issues.update({
-      owner,
-      repo,
-      issue_number: pr_number,
-      milestone: milestones[milestones.length - 1],
-      labels,
-    });
+    // // Add milestone to PR
+    // await octokit.rest.issues.update({
+    //   owner,
+    //   repo,
+    //   issue_number: pr_number,
+    //   milestone: milestones[milestones.length - 1],
+    //   labels,
+    // });
   } catch (err) {
     core.setFailed(err.message);
   }
