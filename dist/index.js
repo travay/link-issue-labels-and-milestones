e56f3a6992ab6b4c0ae91014f218e314112e78b3
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-07-19 00:01:34 +0200

    verifying comment logic for action PR

--- dist/index.js
+++ dist/index.js
@@ -9081,7 +9081,7 @@ const main = async () => {
             },
         });
         const linkedIssues = data?.repository?.pullRequest?.closingIssuesReferences?.nodes;
-        console.log("LINKED ISSUES: ", linkedIssues);
+        console.log("LINKED ISSUES: new value", linkedIssues);
         if (!linkedIssues || !linkedIssues.length) {
             throw Error("Could not find linked issues");
         }
